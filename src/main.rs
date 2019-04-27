791e51a1773476f13db5c077156d0a6aba795b95
Jordi Domènech <jdomenechbborges@gmail.com>
2019-04-28 01:59:34 +0200

    Added menu for the App

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,58 @@ extern crate proc_macro;
 
 use infrastructure::domain::repositories::InMemoryClientRepository;
 use application::get_client_use_case::{Handler, Request};
+use std::io;
+use std::io::Write;
 
 mod domain;
 mod application;
 mod infrastructure;
 
+fn menu() -> u8 {
+    println!("\nMENU");
+    println!("----------------------------------------\n");
+    println!("Please, select an option:");
+    println!("\t 1. Read a client");
+    println!("\t 0. Exit");
+    print!("\nOption: ");
+    io::stdout().flush().expect("Error flushing");
+
+    let mut option :String = String::new();
+
+    io::stdin().read_line(&mut option)
+        .expect("Failed to read line");
+
+    let option: u8 = option.trim().parse()
+        .expect("Please type a number!");
+
+    option
+}
+
 fn main() {
     let client_repository : InMemoryClientRepository = InMemoryClientRepository::new();
     let get_client_use_case : Handler = Handler::new(&client_repository);
 
-    let get_client_use_case_req : Request = Request::new("ID0002");
+    while {
+        let option = menu();
 
-    get_client_use_case.execute(get_client_use_case_req);
+        match option {
+            1 => {
+                println!("\nPlease, enter the ID of the client that you want to read;");
+
+                let mut client_id :String = String::new();
+
+                io::stdin().read_line(&mut client_id)
+                    .expect("Failed to read line");
+
+                let get_client_use_case_req : Request = Request::new(client_id.trim());
+
+                get_client_use_case.execute(get_client_use_case_req);
+            }
+
+            0 => println!("Exiting..."),
+            _ => println!("Invalid option")
+        };
+
+        option != 0
+    } {}
 }
